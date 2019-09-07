3d351e968a5013f843883f0a95c23a1012dd55f5
Tadeas Petak <tadeas@oredev.org>
2019-09-07 21:31:55 +0200

    Initial commit.

--- src/components/pages/PortfolioPage/index.stories.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/diegohaz/arc/wiki/Storybook
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import PortfolioPage from '.'
-
-storiesOf('PortfolioPage', module)
-  .add('default', () => (
-    <PortfolioPage />
-  ))
