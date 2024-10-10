e265a84892ff9c04e00a25a5d4a4ee5a27f9a092
Tadeáš Peták <tadeaspetak@gmail.com>
2024-10-10 15:15:46 +0200

    feat: improve link structure in the header

--- app/components/Shell.tsx
+++ app/components/Shell.tsx
@@ -9,10 +9,34 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import profileImage from "~/images/profile.jpg";
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+const links: { href: string; title: string; icon: IconProp }[] = [
+  {
+    href: "https://www.linkedin.com/in/tade%C3%A1%C5%A1-pet%C3%A1k-2846853a/",
+    title: "The Official Stuff",
+    icon: faLinkedin,
+  },
+  {
+    href: "https://github.com/tadeaspetak",
+    title: "Semicolons",
+    icon: faGithub,
+  },
+  {
+    href: "https://www.goodreads.com/user/show/7310589-tadeas-petak",
+    title: "Letterings",
+    icon: faGoodreads,
+  },
+  {
+    href: "https://medium.com/@tadeaspetak",
+    title: "Scribbles",
+    icon: faMedium,
+  },
+];
 
 export const Shell: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div>
+    <>
       <header className="flex justify-center w-full p-2 bg-white dark:bg-slate-900 shadow-lg sm:fixed sm:p-3">
         <div className="flex items-center justify-between w-full max-w-xl mx-2">
           <button
@@ -28,45 +52,23 @@ export const Shell: React.FC<{ children: ReactNode }> = ({ children }) => {
             <span className="hidden sm:block">Tadeáš Peták</span>
           </button>
           <div className="flex items-center justify-center space-x-2 text-xl">
-            <a
-              href="https://www.linkedin.com/in/tade%C3%A1%C5%A1-pet%C3%A1k-2846853a/"
-              title="The Official Stuff"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <a
-              href="https://github.com/tadeaspetak"
-              title="Semicolons"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a
-              href="https://www.goodreads.com/user/show/7310589-tadeas-petak"
-              title="Letterings"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faGoodreads} />
-            </a>
-            <a
-              href="https://medium.com/@tadeaspetak"
-              title="Scribbles"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faMedium} />
-            </a>
+            {links.map((link) => (
+              <a
+                key={link.href}
+                href={link.href}
+                title={link.title}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={link.icon} />
+              </a>
+            ))}
           </div>
         </div>
       </header>
-      <main className="flex flex-col items-center text-gray-900 sm:pt-12">
+      <main className="flex flex-col items-center text-gray-900 sm:pt-12 mb-16">
         <div className="flex flex-col max-w-xl mx-3">{children}</div>
       </main>
-      <footer className="mt-8"></footer>
-    </div>
+    </>
   );
 };
